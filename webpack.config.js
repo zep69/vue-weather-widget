917584c24856021b8484aa5abf92e8e019348655
nikit <nikita0609991@gmail.com>
2025-11-30 22:17:57 +0400

    edit webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const { VueLoaderPlugin } = require('vue-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { DefinePlugin } = require('webpack');
 
 module.exports = (env, argv) => {
   const isDev = argv.mode === 'development';
@@ -26,70 +27,40 @@ module.exports = (env, argv) => {
     module: {
       rules: [
         {
-            test: /\.vue$/,
-            loader: 'vue-loader',
-            options: {
-                customElement: true
-         }
+          test: /\.vue$/,
+          loader: 'vue-loader',
+          options: { customElement: true }
         },
         {
           test: /\.ts$/,
           use: [
-            {
-              loader: 'ts-loader',
-              options: { appendTsSuffixTo: [/\.vue$/], transpileOnly: false }
-            }
+            { loader: 'ts-loader', options: { appendTsSuffixTo: [/\.vue$/], transpileOnly: false } }
           ],
           exclude: /node_modules/
         },
-        // SCSS inside .vue and imported scss
         {
           test: /\.scss$/i,
           use: [
-            'vue-style-loader', // injects into DOM, supports HMR for .vue
+            'vue-style-loader',
             { loader: 'css-loader', options: { sourceMap: isDev } },
             { loader: 'sass-loader', options: { sourceMap: isDev } }
           ]
         },
-        // plain CSS (e.g. node_modules css)
-        {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader']
-        },
-        // SVG as React-like component (optional), but treat by default as asset/resource
-        {
-          test: /\.svg$/i,
-          type: 'asset/resource',
-          generator: {
-            filename: 'assets/[name].[hash:8][ext]'
-          }
-        },
-        // images/fonts
-        {
-          test: /\.(png|jpe?g|gif|webp|ico|woff2?|eot|ttf|otf)$/i,
-          type: 'asset/resource',
-          generator: {
-            filename: 'assets/[name].[hash:8][ext]'
-          }
-        },
-        // JS through Babel to target modern ES (ES2016)
+        { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
+        { test: /\.svg$/i, type: 'asset/resource', generator: { filename: 'assets/[name].[hash:8][ext]' } },
+        { test: /\.(png|jpe?g|gif|webp|ico|woff2?|eot|ttf|otf)$/i, type: 'asset/resource', generator: { filename: 'assets/[name].[hash:8][ext]' } },
         {
           test: /\.m?js$/i,
           exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: [['@babel/preset-env', { targets: { esmodules: true }, modules: false }]]
-            }
-          }
+          use: { loader: 'babel-loader', options: { presets: [['@babel/preset-env', { targets: { esmodules: true }, modules: false }]] } }
         }
       ]
     },
     plugins: [
       new VueLoaderPlugin(),
-      new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, 'public', 'index.html'),
-        inject: 'body'
+      new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public/index.html'), inject: 'body' }),
+      new DefinePlugin({
+        __VUE_PROD_DEVTOOLS__: 'false', 
       })
     ],
     devtool: isDev ? 'eval-cheap-module-source-map' : 'source-map',
